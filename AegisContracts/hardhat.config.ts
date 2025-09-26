c33d4e45187b4fa48763d3d404fbc097d213ad69
MasterK0927 <keshav.agarwal.12325@gmail.com>
2025-09-27 05:29:59 +0530

    feat: updated harhat config

--- AegisContracts/hardhat.config.ts
+++ AegisContracts/hardhat.config.ts
@@ -1,41 +1,20 @@
-import type { HardhatUserConfig } from "hardhat/config";
-
-import hardhatToolboxMochaEthersPlugin from "@nomicfoundation/hardhat-toolbox-mocha-ethers";
-import { configVariable } from "hardhat/config";
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox-mocha-ethers";
+import "@nomicfoundation/hardhat-verify";
+import "dotenv/config";
 
 const config: HardhatUserConfig = {
-  plugins: [hardhatToolboxMochaEthersPlugin],
-  solidity: {
-    profiles: {
-      default: {
-        version: "0.8.28",
-      },
-      production: {
-        version: "0.8.28",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200,
-          },
-        },
-      },
+  solidity: "0.8.20",
+  networks: {
+    mumbai: {
+      type: "http",
+      url: process.env.POLYGON_MUMBAI_RPC_URL || "",
+      accounts: [process.env.PRIVATE_KEY || ""],
+      chainId: 80001,
     },
   },
-  networks: {
-    hardhatMainnet: {
-      type: "edr-simulated",
-      chainType: "l1",
-    },
-    hardhatOp: {
-      type: "edr-simulated",
-      chainType: "op",
-    },
-    sepolia: {
-      type: "http",
-      chainType: "l1",
-      url: configVariable("SEPOLIA_RPC_URL"),
-      accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
-    },
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_API_KEY,
   },
 };
 
